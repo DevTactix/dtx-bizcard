55fd8c7722a103602701257e801c419fe560f2c8
Thomas Sandberg (DevTactix) <mr.thomas.sandberg@gmail.com>
2018-11-11 21:49:03 +0100

    Add banner with random font-type

--- index.js
+++ index.js
@@ -3,15 +3,36 @@ const Inquirer = require('inquirer');
 const Opn = require('opn');
 const businessCard = require('./data/business-card.json');
 // console.log(businessCard);
-
+console.log(Opn);
 const Actions = {
     BitBucket() { Opn(businessCard.socialMedia.BitBucket.url); },
-    Blog()      { console.log(businessCard.socialMedia.Blog.url); Opn(businessCard.socialMedia.Blog.url); },
+    Blog()      { console.log(businessCard.socialMedia.Blog.url); 
+        console.log( Opn(businessCard.socialMedia.Blog.url).then(console.log('Done!')) ); },
     GitHub()    { Opn(businessCard.socialMedia.GitHub.url); },
     LinkedIn()  { Opn(businessCard.socialMedia.LinkedIn.url); },
     Twitter()   { Opn(businessCard.socialMedia.Twitter.url); },
     YouTube()   { Opn(businessCard.socialMedia.YouTube.url); },
 };
+
+const FontTypes = [
+    'block',
+    'chrome',
+    'simple',
+    'simple3d',
+    'simpleBlock'
+];
+
+function getFontType() {
+    return FontTypes[
+        Math.floor(Math.random() * FontTypes.length)
+    ];
+};
+
+CFonts.say('DevTactix', {
+    font: getFontType(),
+    colors: ['yellowBright', 'blue', 'yellowBright'],
+    background: 'transparent'
+});
 
 Inquirer
     .prompt([{
